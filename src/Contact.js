9d430477073ef1508bf097ee458f55f3c3446e3b
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-22 20:03:45 +0300

    adding remix icon library

--- src/Contact.js
+++ src/Contact.js
@@ -6,12 +6,12 @@ function Contact(props) {
             <img src= {props.img} alt='cat' />
             <h3>{props.name}</h3>
             <div className='info-group'>
-                <i></i>
+                <i class="ri-phone-fill"></i>
                 <p>{props.phone}</p>
             </div>
 
             <div className='info-group'>
-                <i></i>
+                <i class="ri-mail-fill"></i>
                 <p>{props.email}</p>
             </div>
         </div>
