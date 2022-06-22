8f886f3c9079f5393de5865e544d7a00e61fe1e0
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-22 20:01:33 +0300

    adding code to contact card;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Contact from './Contact';
 
+import img1 from './img/cat-1.jpg'
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact 
+      img={img1}
+      name= 'Mr. Whiskerson'
+      phone= '(212) 555 1234'
+      email= 'mr.whiskerson@gmail.com'
+      />
       <Contact />
       <Contact />
       <Contact />
--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,19 @@
 import React from 'react'
-function Contact() {
+
+function Contact(props) {
     return (
-        <div className='contacts'>
+        <div className='contact-card'>
+            <img src= {props.img} alt='cat' />
+            <h3>{props.name}</h3>
+            <div className='info-group'>
+                <i></i>
+                <p>{props.phone}</p>
+            </div>
+
+            <div className='info-group'>
+                <i></i>
+                <p>{props.email}</p>
+            </div>
         </div>
     )
 }
