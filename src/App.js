2ca923392134d0c515927e2d82e96fe1f3d083a3
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-22 19:00:31 +0300

    prapring project files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
     </div>
   );
 }
